1af0d0cc815d74218f24d1c9cc629e6f56f38462
Waldo Feliz <waldofeliz1992@gmail.com>
2019-12-04 08:49:15 -0400

    Inclusion Menu

--- FragmentosKotlin/app/src/main/java/com/android/fragmentoskotlin/MainActivity.kt
+++ FragmentosKotlin/app/src/main/java/com/android/fragmentoskotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.android.fragmentoskotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonInicio.setOnClickListener{
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
