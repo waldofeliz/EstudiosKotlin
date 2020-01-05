c55eaba6d3f28bba20fcb274f337f4b3720309d6
Waldo Feliz <waldofeliz1992@gmail.com>
2020-01-04 20:06:58 -0400

    Design

--- EjemploServicio/app/src/main/java/com/android/ejemploservicio/ui/ServicioActivity.kt
+++ EjemploServicio/app/src/main/java/com/android/ejemploservicio/ui/ServicioActivity.kt
@@ -4,12 +4,15 @@ import AnimeDetail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.android.ejemploservicio.R
 import com.android.ejemploservicio.viewmodel.AnimeViewModel
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_servicio.*
 import java.io.IOException
 
 class ServicioActivity : AppCompatActivity() {
@@ -18,6 +21,7 @@ class ServicioActivity : AppCompatActivity() {
     private lateinit var tipoTv : TextView
     private lateinit var episodiosTv : TextView
     private lateinit var scoreTv : RatingBar
+    private lateinit var imgImagen : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,7 @@ class ServicioActivity : AppCompatActivity() {
             tipoTv = findViewById(R.id.tvTipo)
             episodiosTv = findViewById(R.id.tvEpisodios)
             scoreTv = findViewById(R.id.rbScore)
+            imgImagen = findViewById(R.id.imgAnime)
 
             animeViewModel.resultado.observe(this, Observer<AnimeDetail?>{anime ->
 
@@ -43,6 +48,7 @@ class ServicioActivity : AppCompatActivity() {
                 tipoTv.text = anime?.type.toString()
                 episodiosTv.text = anime?.episodes.toString()
                 scoreTv.rating = anime?.score?.toFloat()?: 0.0f
+                Glide.with(this).load(anime?.image_url).into(imgImagen)
 
             } )
 
